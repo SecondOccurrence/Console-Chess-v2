f3d8210af3227e20e1d9055e6f6ea17264cdf280
SecondOccurrence <jsmith973@protonmail.com>
2024-06-27 22:56:30 +0800

    Implement standard pawn movement validation

--- src/game/pieces/pawn.rs
+++ src/game/pieces/pawn.rs
@@ -22,7 +22,25 @@ impl Pawn {
 
 impl Piece for Pawn {
     fn possible_moves(&mut self, initial_pos: &Position) -> Vec<Position> {
-        let moves = Vec::new();
+        let mut moves = Vec::new();
+        let increment;
+        if self.side == Side::WHITE {
+            increment = 1;
+        }
+        else {
+            increment = -1;
+        }
+
+        if self.first_move {
+            let (new_x, new_y) = (initial_pos.x, initial_pos.y + (2 * increment));
+            let new_move = Position{ x: new_x, y: new_y };
+            moves.push(new_move);
+            self.first_move = false;
+        }
+
+        let (new_x, new_y) = (initial_pos.x, initial_pos.y + increment);
+        let new_move = Position{ x: new_x, y: new_y };
+        moves.push(new_move);
 
         return moves;
     }
